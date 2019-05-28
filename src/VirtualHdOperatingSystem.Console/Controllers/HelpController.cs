1e83f2e1aa22ae329f1f4d0794b466a076134054
João Victor Serralha <joaoserralha_@hotmail.com>
2019-05-28 19:50:48 -0300

    Adicionando mais opcoes ao metodo help

--- src/VirtualHdOperatingSystem.Console/Controllers/HelpController.cs
+++ src/VirtualHdOperatingSystem.Console/Controllers/HelpController.cs
@@ -30,6 +30,54 @@ namespace VirtualHdOperatingSystem.Console.Controllers
                 case "createfolder":
                     System.Console.WriteLine("Cria um diretorio...");
                     break;
+                case "createhd":
+                    System.Console.WriteLine("Cria um HD...");
+                    break;
+                case "selecthd":
+                    System.Console.WriteLine("Seleciona um HD...");
+                    break;
+                case "createfile":
+                    System.Console.WriteLine("Cria um Arquivo...");
+                    break;
+                case "typehd":
+                    System.Console.WriteLine("Exibe Conteudo do HD...");
+                    break;
+                case "cd":
+                    System.Console.WriteLine("Alterna entre pastas...");
+                    break;
+                case "statushd":
+                    System.Console.WriteLine("Exibe propriedades do HD...");
+                    break;
+                case "removehd":
+                    System.Console.WriteLine("Remove HD...");
+                    break;
+                case "copyfrom":
+                    System.Console.WriteLine("Copia arquivo JPG do HD REAL para HD VIRTUAL...");
+                    break;
+                case "copyto":
+                    System.Console.WriteLine("Exibe Conteudo do HD...");
+                    break;
+                case "renamedir":
+                    System.Console.WriteLine("Renomeia diretorio...");
+                    break;
+                case "copy":
+                    System.Console.WriteLine("Copia arquivo ou pasta...");
+                    break;
+                case "move":
+                    System.Console.WriteLine("Move arquivo ou pasta...");
+                    break;
+                case "help":
+                    System.Console.WriteLine("Ajuda sobre comandos...");
+                    break;
+                case "rename":
+                    System.Console.WriteLine("Renomeia arquivo...");
+                    break;
+                case "rmdir":
+                    System.Console.WriteLine("Remove diretorio...");
+                    break;
+                case "tree":
+                    System.Console.WriteLine("Exibe estrutra de diretorios...");
+                    break;
                 default:
                     System.Console.WriteLine("Comando não encontrado!");
                     break;
